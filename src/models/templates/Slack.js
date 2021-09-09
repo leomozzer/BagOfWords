4f44d8624bd4e77054ba154e20aab7bdbed8c981
leomozzer <mozzer.ferraz@gmail.com>
2021-09-08 22:14:21 -0300

    Change Slack template to handle with examples

--- src/models/templates/Slack.js
+++ src/models/templates/Slack.js
@@ -1,32 +1,42 @@
-function GetExample(meanings) {
-    let example = ""
-    meanings.find(items => {
-        items['definitions'].find(def => {
-            if (def['example']) { example = def['example'] }
-        })
-    })
-    return example.charAt(0).toUpperCase() + example.slice(1)
-}
+const { GetExample } = require("../../handlers/WordHandler");
 
 module.exports = {
     SlackTemplate(words) {
         let blocks = []
         for (const word of words) {
             let items = [];
-            items.push(`*Word*: ${word['word']}`) //Add the word into the array
-            items.push(`*Example*: ${GetExample(word['meanings'])}`) //Add the Example into the array; Get the first example with value
-            if (word['phonetics'][0]['audio']) {
-                items.push(`*Phonetics*: <${!word['phonetics'][0]['audio'].includes("http") ? `https:${word['phonetics'][0]['audio']}` : `${word['phonetics'][0]['audio']}`}|${word['word']}>`) //Add the word into the array
-            }
-            blocks.push({
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": `${items.join("\n")}`
+            if (word['translation']) {
+                items.push(`*Word*: ${word['word']} - ${word['translation']['word']}`) //Add the word into the array
+                word['translation']['example'] ? items.push(`*Example*: ${GetExample(word['meanings'])} \n ${word['translation']['example']}`) : items.push(`*Example*: ${GetExample(word['meanings'])}`)
+                if (word['phonetics'][0]['audio']) {
+                    items.push(`*Phonetics*: <${!word['phonetics'][0]['audio'].includes("http") ? `https:${word['phonetics'][0]['audio']}` : `${word['phonetics'][0]['audio']}`}|${word['word']}>`) //Add the word into the array
                 }
-            }, {
-                "type": "divider"
-            })
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `${items.join("\n")}`
+                    }
+                }, {
+                    "type": "divider"
+                })
+            }
+            else {
+                items.push(`*Word*: ${word['word']}`) //Add the word into the array
+                items.push(`*Example*: ${GetExample(word['meanings'])}`) //Add the Example into the array; Get the first example with value
+                if (word['phonetics'][0]['audio']) {
+                    items.push(`*Phonetics*: <${!word['phonetics'][0]['audio'].includes("http") ? `https:${word['phonetics'][0]['audio']}` : `${word['phonetics'][0]['audio']}`}|${word['word']}>`) //Add the word into the array
+                }
+                blocks.push({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `${items.join("\n")}`
+                    }
+                }, {
+                    "type": "divider"
+                })
+            }
         }
         return blocks
     }

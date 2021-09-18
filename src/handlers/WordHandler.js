2a9ee896651fedf60a0e75ef7bb511c8f2662aba
leomozzer <mozzer.ferraz@gmail.com>
2021-09-18 11:05:53 -0300

    Fixed the GetExample is not defined

--- src/handlers/WordHandler.js
+++ src/handlers/WordHandler.js
@@ -1,16 +1,18 @@
 const { WordDefinition } = require("../api/Dictionary");
 const { MicrosoftTranslate } = require("../models/Translate");
 
-module.exports = {
-    GetExample(meanings) {
-        let example = ""
-        meanings.find(items => {
-            items['definitions'].find(def => {
-                if (def['example']) { example = def['example'] }
-            })
+function GetExample(meanings) {
+    let example = ""
+    meanings.find(items => {
+        items['definitions'].find(def => {
+            if (def['example']) { example = def['example'] }
         })
-        return example.charAt(0).toUpperCase() + example.slice(1)
-    },
+    })
+    return example.charAt(0).toUpperCase() + example.slice(1)
+}
+
+module.exports = {
+    GetExample,
     async HandleWords(words, language = "en") {
         let BagOfWords = [];
         for (const word of words) {

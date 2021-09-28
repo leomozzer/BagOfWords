3be58e4e27573d6fa35229a016b0890f99ff30c3
leomozzer <mozzer.ferraz@gmail.com>
2021-09-28 19:38:10 -0300

    changed the tests

--- __tests__/integration/Word.test.js
+++ __tests__/integration/Word.test.js
@@ -2,9 +2,18 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('Words Routes', () => {
-    it('Word', async () => {
-        const res = await request(app)
-            .get('/word')
-        expect(res.status).toBe(200)
+    test("Word", () => {
+        const responseObject = {}
+        const response = {
+            json: jest.fn().mockImplementation((result) => {
+                console.log(result)
+                responseObject = result
+            })
+        }
     })
+    // it('Word', async () => {
+    //     const res = await request(app)
+    //         .get('/word')
+    //     expect(res.status).toBe(200)
+    // })
 })

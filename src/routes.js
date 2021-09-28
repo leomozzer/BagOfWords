ee3c5745d08549e0f15cb1cc3103595e3b26b950
leomozzer <mozzer.ferraz@gmail.com>
2021-09-28 18:10:46 -0300

    Added .env verification

--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,22 @@ const { GetWords, GetWordsSlackTemplate, RandomWordsLanguage } = require('./cont
 routes.get('/word', GetWords);
 
 routes.get('/word/template/slack/', GetWordsSlackTemplate)
-routes.get('/word/template/slack/:language', GetWordsSlackTemplate)
+routes.get('/word/template/slack/:language', (req, res, next) => {
+    if (!process.env.MICROSOFT_TRANSLATOR_SUBSCRIPTION_KEY) {
+        return res.json({
+            "Error": "Missing credentials, check the .env file"
+        })
+    }
+    next();
+}, GetWordsSlackTemplate)
 
-routes.get('/word/:language', RandomWordsLanguage)
+routes.get('/word/:language', (req, res, next) => {
+    if (!process.env.MICROSOFT_TRANSLATOR_SUBSCRIPTION_KEY) {
+        return res.json({
+            "Error": "Missing credentials, check the .env file"
+        })
+    }
+    next();
+}, RandomWordsLanguage)
 
 module.exports = routes;

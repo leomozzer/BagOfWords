4f904a6fe04cd5365f5cb68c2b38dbc70c3cf9cb
leomozzer <mozzer.ferraz@gmail.com>
2021-09-29 20:32:52 -0300

    removed request.bind

--- __tests__/integration/Word.test.js
+++ __tests__/integration/Word.test.js
@@ -2,9 +2,7 @@ const { default: axios } = require('axios');
 const request = require('supertest');
 const app = require('../../src/app');
 
-const server = app.listen(80, "127.0.0.1", () => { })
-
-let requestBind = request.bind(request, "http://127.0.0.1:80")
+const server = app.listen(80, "http://127.0.0.1", () => { })
 
 // const server = app.listen(80, function () {
 //     console.log('App listening on port ' + 80)
@@ -55,9 +53,8 @@ describe('Words Routes', () => {
         // const agent = request.agent(server)
         // const res = await agent.get("/word")
         // console.log(res)
-        const res = await requestBind(server)
+        const res = await request(server)
             .get('/word')
-        // console.log(res)
         expect(res.status).toBe(200)
     })
 })

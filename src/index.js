c752e78aac43512b066c21e78161fe92c2e10557
leomozzer <mozzer.ferraz@gmail.com>
2021-09-29 18:59:13 -0300

    added docker with tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const app = require('../app');
+const app = require('./app');
 require('dotenv').config()
 app.listen(process.env.PORT, () => {
     console.log(`App running on port ${process.env.PORT}`)

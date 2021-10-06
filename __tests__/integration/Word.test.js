2fe4815216916bcd24497296309b66c58520e449
leomozzer <mozzer.ferraz@gmail.com>
2021-10-06 19:59:00 -0300

    testing the tests

--- __tests__/integration/Word.test.js
+++ __tests__/integration/Word.test.js
@@ -2,8 +2,9 @@ const request = require('supertest');
 const app = require('../../src/app');
 
 describe('Words Routes', () => {
+    const server = app.listen(80, "127.0.0.1")
     it('Word', async () => {
-        const res = await request(app)
+        const res = await request(server)
             .get('/word')
         expect(res.status).toBe(200)
     })

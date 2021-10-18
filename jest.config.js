5cec77c151676d5ac6a9066795d2650f38d61f38
leomozzer <mozzer.ferraz@gmail.com>
2021-10-18 19:50:33 -0300

    updated the handler

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   // automock: false,
 
   // Stop running tests after `n` failures
-  bail: false,
+  bail: true,
 
   // The directory where Jest should store its cached dependency information
   // cacheDirectory: "C:\\Users\\Leonardo\\AppData\\Local\\Temp\\jest",
